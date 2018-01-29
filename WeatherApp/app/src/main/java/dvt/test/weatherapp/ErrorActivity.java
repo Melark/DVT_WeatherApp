052707039d1dd62073390a87daf6cc3fe79ddf94
Unknown <markarrow6@gmail.com>
2018-01-29 20:32:23 +0200

    Small layout changes
    Changes made to change the layout so that it looks better, and added some color identifiers

--- WeatherApp/app/src/main/java/dvt/test/weatherapp/ErrorActivity.java
+++ WeatherApp/app/src/main/java/dvt/test/weatherapp/ErrorActivity.java
@@ -1,11 +1,16 @@
 package dvt.test.weatherapp;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by marka on 2018/01/29.
  */
 
 public class ErrorActivity extends Activity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.error_layout);
+    }
 }
